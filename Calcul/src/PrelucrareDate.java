803dd92cd22ee20c8e61469cfa4c578be631ae90
AlexIvanCPA <alexandruivan311@gmail.com>
2019-04-01 17:36:21 +0300

    Add files via upload

---
+++ Calcul/src/PrelucrareDate.java
@@ -0,0 +1,33 @@
+public class PrelucrareDate {
+
+    //utilizeaza functii definite de modulul Gestionare pentru a extrage din baza de date variabilele mentionate
+    public void primesteFormula(String id_materie, String[] formula, String[] antet)
+    {
+
+    }
+
+    //creeaza cu ajutorul unei functii definite de modulul Gestionare o noua tabela in baza de date pentru o anumita materie
+    public void trimiteAntet(String id_materie, String formula, String antet)
+    {
+
+    }
+
+    //extrage notele fiecarei materii ce contine id_student si populeaza variabila materii din PaginaStudent
+    public void primesteDateStudent(String id_student)
+    {
+
+    }
+
+    //aplica formulele pentru materiile unde s-au adaugat note
+    public void updateBazaDeDate(String id_materie)
+    {
+
+    }
+
+
+
+
+
+
+
+}
