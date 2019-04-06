de0378dc258eee57e4fca0e94f28dd1fe1aeed4a
Adina Ciubotaru <adina_ciubotaru98@yahoo.com>
2019-04-07 00:40:48 +0300

    Creare formula postfixata si cateva update-uri pentru teste

--- Calcul/src/Formula.java
+++ Calcul/src/Formula.java
@@ -1,6 +1,8 @@
+import java.util.Stack;
+
 public class Formula {
     public String formula;
-    public String postfixFormula;
+    public String formulaPostfixata="";
     public int nrVariabile=0;
     public String criteriiPromovare;
     public String[] variabile= new String[50];
@@ -12,9 +14,12 @@ public class Formula {
         this.formula=formula;
     }
 
+
+    //used only for tests
     public void setFormula(String formula) {
         this.formula=formula;
         nrVariabile=0;
+        formulaPostfixata="";
     }
 
     //primeste un index de unde incepe paranteza pentru sum, returneaza  pozitia primei paranteze inchise pe care o gaseste daca functia sum a fost folosita corect si -1 altfel
@@ -49,7 +54,7 @@ public class Formula {
         int nrParantezeDeschise=0;
 
 
-        String var=new String("");
+        String var="";
 
 
         for( i=0; i<formula.length(); i++)
@@ -167,6 +172,12 @@ public class Formula {
 
            }
 
+           else
+           {
+               mesajPentruFront="Eroare: Caracter nepermis";
+               break;
+           }
+
         }
 
         if(i==formula.length())
@@ -179,7 +190,95 @@ public class Formula {
 
     }
 
+    void infixToPostfix()
+    {
+        boolean justReadAVariable=false;
+        Stack<String> stack = new Stack<>();
+        stack.push("#");
+        int l=formula.length();
 
+        for(int i=0; i<l; i++)
+        {
+            justReadAVariable=false;
+            while(i<l && ((formula.charAt(i) >= 'a' && formula.charAt(i) <= 'z')||(formula.charAt(i) >= 'A' && formula.charAt(i) <= 'Z') || (formula.charAt(i) >= '0' && formula.charAt(i) <= '9'))){
+                formulaPostfixata+=formula.charAt(i);
+                i++;
+
+                justReadAVariable=true;
+            }
+            if(justReadAVariable)
+            {
+                if(formulaPostfixata.length()>=3)
+                    if(formula.charAt(i-1)=='m' && formula.charAt(i-2)=='u' && formula.charAt(i-3)=='s')
+                    {
+                        if(formulaPostfixata.length()==3) formulaPostfixata="";
+                        else{
+                            String deleteSumString=formulaPostfixata.substring(0,formulaPostfixata.length()-4);
+                            formulaPostfixata=deleteSumString;
+                        }
+                    }
+                formulaPostfixata+=" ";
+            }
+
+            if(i>=l)
+                break;
+
+            if(formula.charAt(i)=='(')
+                stack.push("(");
+
+            else if(formula.charAt(i)==')')
+            {
+                while(!stack.peek().equals("#") && !stack.peek().equals("("))
+                {
+                    formulaPostfixata+=stack.peek();
+                    stack.pop();
+                }
+
+                if(stack.peek().equals("("))
+                    stack.pop();
+            }
+
+            else if("+-/*:".indexOf(formula.charAt(i))>=0)
+            {   String currentOperator= "";
+                currentOperator+=formula.charAt(i);
+                while(!stack.peek().equals("#") && precedence(currentOperator)<=precedence(stack.peek()))
+                {
+                    formulaPostfixata+=stack.peek();
+                    stack.pop();
+                }
+
+                stack.push(currentOperator);
+            }
+        }
+
+        while(!stack.peek().equals("#"))
+        {
+            formulaPostfixata+=stack.peek();
+            stack.pop();
+        }
+
+
+
+    }
+
+    int precedence(String operator)
+    {
+        switch(operator)
+        {
+            case ":":
+                return 3;
+            case "*":
+                return 2;
+            case "/":
+                return 2;
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+            default:
+                return -1;
+        }
+    }
 
 
 
--- Calcul/tests/FormulaTest.java
+++ Calcul/tests/FormulaTest.java
@@ -10,6 +10,7 @@ public class FormulaTest {
         MockFormula.parsareFormula();
         assertEquals("L", MockFormula.variabile[0]);
         assertEquals("E", MockFormula.variabile[1]);
+        assertEquals(2, MockFormula.nrVariabile);
         assertEquals("Formula este valida", MockFormula.mesajPentruFront);
 
 
@@ -31,6 +32,7 @@ public class FormulaTest {
         assertEquals("E", MockFormula.variabile[2]);
         assertEquals("Formula este valida", MockFormula.mesajPentruFront);
 
+
         MockFormula.setFormula("L * T ( P + B)");
         MockFormula.parsareFormula();
         assertEquals("Eroare: Inainte de paranteza nu exista operator", MockFormula.mesajPentruFront);
@@ -46,29 +48,63 @@ public class FormulaTest {
     @Test
     public void verificaUtilizareSum()
     {
-        int rezultat;
+        int pozitieParantezaInchisa;
         Formula MockFormula=new Formula("E+sum(L1:L14)");
 
-        rezultat=MockFormula.verificaUtilizareSum(5);
-        assertEquals(12,rezultat);
+        pozitieParantezaInchisa=MockFormula.verificaUtilizareSum(5);
+        assertEquals(12,pozitieParantezaInchisa);
+        assertEquals(14,MockFormula.nrVariabile);
+        for(int i=0; i<14; i++) // pe pozitia 0 ar trebui sa fie variabila E, dar se apeleaza direct verificaUtilizareSum, asa ca E e ignorat
+        {
+            String var="L";
+            var+=(i+1);
+            assertEquals(var,MockFormula.variabile[i]);
+        }
 
         MockFormula.setFormula("sum(L2:L4)");
-        rezultat=MockFormula.verificaUtilizareSum(3);
-        assertEquals(9,rezultat);
+        pozitieParantezaInchisa=MockFormula.verificaUtilizareSum(3);
+        assertEquals(9,pozitieParantezaInchisa);
+        assertEquals(3,MockFormula.nrVariabile);
+        for(int i=0; i<3; i++)
+        {
+            String var="L";
+            var+=(i+2);
+            assertEquals(var,MockFormula.variabile[i]);
+        }
+
 
         MockFormula.setFormula("sum(L1:E5)");
-        rezultat=MockFormula.verificaUtilizareSum(3);
-        assertEquals(-1,rezultat);
+        pozitieParantezaInchisa=MockFormula.verificaUtilizareSum(3);
+        assertEquals(-1,pozitieParantezaInchisa);
         assertEquals("Eroare: Variabilele din sum nu fac parte din aceeasi componenta", MockFormula.mesajPentruFront);
 
         MockFormula.setFormula("sum(L4:L2)");
-        rezultat=MockFormula.verificaUtilizareSum(3);
-        assertEquals(-1,rezultat);
+        pozitieParantezaInchisa=MockFormula.verificaUtilizareSum(3);
+        assertEquals(-1,pozitieParantezaInchisa);
         assertEquals("Eroare: Ordinea variabilelor din sum nu e corecta", MockFormula.mesajPentruFront);
 
+    }
 
+    @Test
+    public void infixToPostfix()
+    {
+        Formula MockFormula=new Formula("L + E");
+        MockFormula.infixToPostfix();
+        assertEquals("L E +", MockFormula.formulaPostfixata);
 
+        MockFormula.setFormula("L + E* (T+P)");
+        MockFormula.infixToPostfix();
+        assertEquals("L E T P +*+", MockFormula.formulaPostfixata);
+
+        MockFormula.setFormula("L + sum(E1:E2)");
+        MockFormula.infixToPostfix();
+        assertEquals("L E1 E2 :+", MockFormula.formulaPostfixata);
 
 
     }
+
+
+
+
+
 }
