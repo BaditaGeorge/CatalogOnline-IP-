5d79948ec50c414108dfde3b2bb110b7c448c91b
AnaMaria0924 <annaaa_24@yahoo.com>
2019-04-22 02:02:29 +0300

    fubnctia generareAntet

--- Calcul/src/PaginaFormula.java
+++ Calcul/src/PaginaFormula.java
@@ -5,8 +5,8 @@ public class PaginaFormula {
     String mesajPentruFront;
     private PrelucrareDate prelucrareDate;
 
-    public PaginaFormula(PrelucrareDate prelucrareDate){
-        this.prelucrareDate=prelucrareDate;
+    public PaginaFormula(PrelucrareDate prelucrareDate) {
+        this.prelucrareDate = prelucrareDate;
     }
 
     String generareFormula(String id_materie, String formula) {
@@ -14,21 +14,29 @@ public class PaginaFormula {
         return mesajPentruFront;
     }
 
-    void parsareFormule(String formule)
-    {
+    void parsareFormule(String formule) {
 
 
     }
 
     String generareAntet(String id_materie, String antet) {
 
+        //in constructor antet e de tip String[], functia asta e folosita in alte parti cu prototipul asta si am preferat sa convertesc decat sa modific parametrii
+        String[] sir = new String[1];
+        sir[0] = antet;
+        AntetMaterie antetNou = new AntetMaterie(sir);
+        antetNou.verificareAntetMaterie();
+        mesajPentruFront = antetNou.getMesajPentruFront();
+        this.prelucrareDate.functiiGestiune.raspunsDeLaCalcul(mesajPentruFront);
+        if (mesajPentruFront.equals("Antetul este valid"))
+            this.prelucrareDate.functiiGestiune.updateNote(antet, id_materie, "10");  //nu am inteles de unde va fi luat stringul cu note
         return mesajPentruFront;
+
     }
 
     void schimbareAntet(String id_materie, String antet) {
 
     }
-
 
 
     void schimbareFormula(String id_materie, String formula) {
