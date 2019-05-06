3f0d40f353febb28b69c7cb04c9184352531acd2
AlexIvanCPA <alexandruivan311@gmail.com>
2019-05-06 18:44:36 +0300

    Update pentru criterii de promovare

--- Calcul/src/PrelucrareDate.java
+++ Calcul/src/PrelucrareDate.java
@@ -12,6 +12,7 @@ public class PrelucrareDate {
      *                       - "f: id_materie; formula" - va fi instaintiat un obiect PaginaForumla si apelata functia generareFormula(id_materie, formula) <br>
      *                       - "a: id_materie; antet" - va fi instantiat un obiect PaginaFormula si apeleata functia generareAntet(id_materie, antet)<br>
      *                       - "n: id_student; id_materie; note" - va fi apelata functia String selectFormula(String id_materie) si instantiat un obiect Calcul cu stringul primit drept formula. Apoi va fi apelata functia parsareNote din Calcul ce va returna notele updatate si in final va fi apelata updateNote(String id_s, String id_m, String note) in functiiGestiune.
+     *                       - "c: id_materie; criterii" va fi asemanator cu primirea mesajului cu formula,  se instantiaza un obiect PaginaFormula si se apeleaza functia generareCriterii
      * @return "Update efectuat" - daca formatul este corect <br>
      * "Solicitarea pentru calcul nu are formatul dorit" - daca formatul este incorect <br>
      * "Nu se vor face calcule fara formula" - daca formula lipseste
@@ -20,7 +21,7 @@ public class PrelucrareDate {
 
     public String primesteMesajFront(String mesajDeLaFront) {
         int i = 2;
-        if (mesajDeLaFront.charAt(0) == 'f' || mesajDeLaFront.charAt(0) == 'a') {
+        if (mesajDeLaFront.charAt(0) == 'f' || mesajDeLaFront.charAt(0) == 'a' || mesajDeLaFront.charAt(0) == 'c') {
             PaginaFormula paginaFormula = new PaginaFormula(this);
 
             String id_materie = "";
@@ -47,6 +48,15 @@ public class PrelucrareDate {
                     i++;
                 }
                 return paginaFormula.generareAntet(id_materie, antet);
+                
+            } else if (mesajDeLaFront.charAt(0) == 'c'){
+                String criterii ="";
+                while (mesajDeLaFront.charAt(i) == ' ') i++;
+                while (i != mesajDeLaFront.length()) {
+                    criterii += mesajDeLaFront.charAt(i);
+                    i++;
+                }
+                return paginaFormula.generareCriterii(id_materie, criterii);
             }
         } else if (mesajDeLaFront.charAt(0) == 'n') {
             String id_student = "", id_materie = "", note = "";
