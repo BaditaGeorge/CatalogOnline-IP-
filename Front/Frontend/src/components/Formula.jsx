35443817dc909115c9c8d712cf1e6738a444fd1a
Emilian Radu <emiradu98@icloud.com>
2019-05-13 15:06:26 +0300

    Formula.css

--- Front/Frontend/src/components/Formula.jsx
+++ Front/Frontend/src/components/Formula.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Row, Col, Form, Button} from "react-bootstrap";
-
+import "../css/Formula.css";
 export default class Formula extends Component {
     constructor(props) {
         super(props);
