0bc8da581efd0e6239502835514af7c4cdfbe337
Emilian Radu <emiradu98@icloud.com>
2019-05-14 10:02:02 +0300

    Register+css

--- Front/Frontend/src/components/Login.jsx
+++ Front/Frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import '../App.css';
 import {Button,Form} from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import '../css/Login.css'
 
 export default class Login extends Component {
     constructor(props) {
@@ -18,26 +19,26 @@ export default class Login extends Component {
 
     render() {
         return (
-            
+
             <Card style={{ width: '20rem', marginTop: '10%',marginLeft:'auto' ,marginRight:'auto' }}>
-            <Card.Body>
-            <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-          
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            
-            <Button  variant="primary" type="submit" block>
-              Login
-            </Button>
-            </Form>
-            </Card.Body>
-           </Card>
+                <Card.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button  variant="primary" type="submit" block>
+                            Login
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         );
     }
 }
--- Front/Frontend/src/components/Register.jsx
+++ Front/Frontend/src/components/Register.jsx
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import '../App.css';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
+import '../css/Register.css'
+import {Button, Form} from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
 export default class Register extends Component {
     constructor(props) {
@@ -16,11 +20,34 @@ export default class Register extends Component {
 
     render() {
         return (
-            <main className="dashboard">
-                <div>
-                    Register Form
-                </div>
-            </main>
+            <Card style={{ width: '20rem', marginTop: '10%',marginLeft:'auto' ,marginRight:'auto' }}>
+                <Card.Body>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicMatr">
+                            <Form.Label>Matricol Number</Form.Label>
+                            <Form.Control type="nr" placeholder="Enter matricol" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button  variant="primary" type="submit" block>
+                            Register
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         );
     }
 }
+
+
+
+
