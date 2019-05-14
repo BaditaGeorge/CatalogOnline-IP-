46fdbaadf0443e7039cd511821a08a55979e1a99
jjspielhurt <mariuta.silviu@yahoo.com>
2019-05-14 03:17:09 +0300

    Sprint 3

---
+++ Security/Final/src/main/java/com/Main.java
@@ -0,0 +1,56 @@
+package com;
+
+import com.database.SQL_func;
+import com.security.*;
+
+import javax.mail.MessagingException;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws MessagingException {
+//        User user1=new User("teodora.ichim","parola123","teodora.ichim@info.uaic.ro");
+//       System.out.println(user1.register());
+
+//        System.out.println(user1.login());
+
+//        Access access=new Access();
+//        System.out.println("Profesor:"+access.isProfesor("Andrei"));
+////        System.out.println("Student:"+access.isStudent("Andrei"));
+        Login  l=new Login();
+        l.createSession("teodora.ichim");
+//        boolean ses_id=l.checkSession("123");
+//        String usern=l.getUsername("123");
+//        System.out.println("id:"+ses_id+" "+usern);
+
+//        HelpFunctions func=new HelpFunctions();
+//        SecureRandom random = new SecureRandom();
+//        byte[] salt = new byte[16];
+//        random.nextBytes(salt);
+//        System.out.println(salt);
+//        System.out.println(func.byteToString(salt));
+//        System.out.println(salt == func.byteToString(salt).getBytes());
+
+//        SimpleDateFormat sd=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+//        try {
+//            //conversie string->date
+//            Date date=sd.parse("1999-09-02 10:22");
+//            Date cur=new Date();
+//            //conversia date->string
+//            String format=sd.format(cur);
+//            System.out.println(format);
+//            //diferenta in ore dintre doua date
+//            long diff=Math.abs(cur.getTime()-date.getTime());
+//            long hours= TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS);
+//
+//            //
+//            System.out.println(hours);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
---
+++ Security/Final/src/main/java/com/security/Login.java
@@ -0,0 +1,116 @@
+package com.security;
+
+//import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import com.database.SQL_func;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Clasa contine functii ce verifica corectitudinea datelor introduse de utilizator la login(respectand un anumit pattern matching)
+ */
+public class Login {
+    private Verify verify = new Verify();
+    private HelpFunctions funct = new HelpFunctions();
+    private SQL_func db = new SQL_func("/home/silviu/JavaProjects/securitate_new2/IP-Securitate/IP-Security/Final/BD_Gestiunea");
+
+    /**
+     * Functia verifica ca username-ul si parola introduse de catre utilizator sa respecte pattern-ul de logare
+     * @param username Parametrul reprezinta username-ul plain-text introdus de utilizator
+     * @param pass Parametrul reprezinta username-ul plain-text introdus de utilizator
+     * @return Returneaza true daca patternul este respectat, false altfel
+     */
+    public boolean login(String username, String pass) {
+        if (username == null || pass == null || username.isEmpty() || pass.isEmpty()) return false;
+        if (verify.verifyAplhaNumeric(username)) {
+            if (db.countUsersByName(username) != 0) {
+                String salt = db.getSalt(username); //comented to test
+//        	byte[] salt = {(byte)0x10};//made for test
+//        	String hash= funct.encrypt(pass, salt);//made for test
+//                System.out.println("salt din db:"+salt+":"+salt.length());
+                System.out.println(pass);
+                String hash = funct.encrypt(pass, salt.getBytes());// comented to test
+                System.out.println(hash);
+                if (db.countUsersByUsernamePass(username, hash) != 0) {
+                    if (db.verificareLogIn(username))
+                        return true;
+                    else System.out.println("Account not activated");
+                } else {
+                    System.out.println("Username/pass invalid");
+                }
+            }
+
+        } else {
+            System.out.println("Username must be alphanumeric.");
+        }
+        return false;
+    }
+
+    public Date stringToDate(String stringDate){
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(stringDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public String currentTime() { //conversia date->string
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String format;
+        Date date;
+        Date current;
+        try {
+            date = sd.parse("1999-09-02 10:22");
+            current = new Date();
+            format = sd.format(current);
+            return format;
+        } catch (Exception e) {
+            return "error" + e;
+        }
+    }
+    public String getUsername(String ses_id)
+    {
+        return db.getUsername(ses_id);
+    }
+    public String createSession(String usernameParam){
+        String hash;
+        do{
+            hash=funct.generateRandomString(16);
+        }while(db.countSession(hash)!=0);
+
+        db.addSession(usernameParam,hash,currentTime());
+        return hash;
+
+
+    }
+    public long hoursPassedSince(Date lastActivity){
+        Date current=new Date();
+        long diffMiliseconds=Math.abs(current.getTime()-lastActivity.getTime());
+        long diffHours = TimeUnit.HOURS.convert(diffMiliseconds, TimeUnit.MILLISECONDS);
+        return diffHours;
+    }
+    public boolean checkSession(String hashedSessionIdParam){
+
+        if(db.countSession(hashedSessionIdParam)!=0){
+            if(hoursPassedSince(stringToDate(db.getTime(hashedSessionIdParam)))<2){
+                SimpleDateFormat sd=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                Date currentDate=new Date();
+                //conversia date->string
+                String currentDateString=sd.format(currentDate);
+                db.updateSessionActivity(hashedSessionIdParam,currentDateString);
+                return true;
+            }else{
+                db.deleteSession(hashedSessionIdParam);
+                return false;
+            }
+        }else return false;
+    }
+
+}
