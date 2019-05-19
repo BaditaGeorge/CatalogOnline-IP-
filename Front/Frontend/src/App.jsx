0d846e33f2d4e7b4a2b1b09446f4b6d315650664
gceico <ceicoschi.gabriel@gmail.com>
2019-05-19 18:01:45 +0300

    admin dashboard fancy ui

--- Front/Frontend/src/App.jsx
+++ Front/Frontend/src/App.jsx
@@ -10,6 +10,13 @@ import {ProfessorDashboardRedux} from './components/ProfessorDashboard';
 import SecretaryDashboard from './components/SecretaryDashboard'
 import AdminDashboard from './components/AdminDashboard';
 import About from './components/About';
+import AdminLayout from './components/Template/Admin.jsx';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/animate.min.css";
+import "./css/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./css/demo.css";
+import "./css/pe-icon-7-stroke.css";
 
 class App extends Component {
     render() {
@@ -25,6 +32,7 @@ class App extends Component {
                             <Route path="/secretary" component={SecretaryDashboard}/>
                             <Route path="/admin" component={AdminDashboard}/>
                             <Route path="/about" component={About}/>
+                            <Route path="/test" render={props => <AdminLayout {...props} />}/>
                         </Switch>
                     </Suspense>
                 </Router>
