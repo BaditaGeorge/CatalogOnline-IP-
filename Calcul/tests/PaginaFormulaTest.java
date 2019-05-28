00e4b860b6ed9fe9905cd1c13ea11968bcba9ff7
Adina Ciubotaru <adina_ciubotaru98@yahoo.com>
2019-05-28 09:41:24 +0300

    Final touch

--- Calcul/tests/PaginaFormulaTest.java
+++ Calcul/tests/PaginaFormulaTest.java
@@ -4,40 +4,7 @@ import static org.junit.Assert.*;
 
 public class PaginaFormulaTest {
 
-    @Test
-    public void generareAntet() {
 
-
-        PrelucrareDate prelucrareDate= new PrelucrareDate();
-
-        /*
-        PaginaFormula paginaFormula = new PaginaFormula(prelucrareDate);
-        paginaFormula.generareAntet("381","L1 L2 WEF");
-        assertEquals("Antetul este valid", paginaFormula.mesajPentruFront);
-
-        paginaFormula.generareAntet("2", "1L");
-        assertEquals("Eroare: Campurile din antet pot contine doar litere si cifre si trebuie sa inceapa cu o litera", paginaFormula.mesajPentruFront);
-        */
-
-       prelucrareDate.primesteMesajFront("f: 10; P=(E+L)/2");
-
-
-    }
-
-    @Test
-    public void generareFormula() {
-        /*
-        PrelucrareDate prelucrareDate= new PrelucrareDate();
-        PaginaFormula paginaFormula = new PaginaFormula(prelucrareDate);
-        paginaFormula.generareFormula("7","L1 = L2 +L3");
-        assertEquals("Eroare: Antetul nu a fost definit", paginaFormula.mesajPentruFront);
-
-        paginaFormula.generareFormula("381","L1 + L2 + L3");
-        assertEquals("Eroare: Antetul nu a fost definit", paginaFormula.mesajPentruFront);
-        */
-
-
-    }
 
     @Test
     public void verificaOrdineFormule() {
